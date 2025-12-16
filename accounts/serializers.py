3d0dd257f66d4177563415a312686a707b515368
MahadSeecs <mahadm.bscs21seecs@seecs.edu.pk>
2025-12-16 17:32:21 +0500

    Added serializer and views with admin permission for User model

---
+++ accounts/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role', 'is_active', 'is_superuser']
+        read_only_fields = ['id', 'is_superuser']
