bfc2beffb185b4936a9f972fbf3d6a9ca5ac5818
MahadSeecs <mahadm.bscs21seecs@seecs.edu.pk>
2025-12-16 15:19:58 +0500

    Changed CharField roles to TextChoices

--- accounts/models.py
+++ accounts/models.py
@@ -4,22 +4,17 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    ROLE_CLIENT = 'client'
-    ROLE_PM = 'pm'
-    ROLE_ENGINEER = 'engineer'
-    ROLE_ADMIN = 'admin'
+    class Role(models.TextChoices):
+        CLIENT = 'client', 'Client'
+        PROJECT_MANAGER = 'project_manager', 'Project Manager'
+        ENGINEER = 'engineer', 'Engineer'
+        SUPER_ADMIN = 'super_admin', 'Super Admin'
 
-    #for better string readibility
-    ROLE_CHOICES = [
-    (ROLE_CLIENT, 'Client'),
-    (ROLE_PM, 'Project Manager'),
-    (ROLE_ENGINEER, 'Engineer'),
-    (ROLE_ADMIN, 'Super Admin'),
-]
-    
     role = models.CharField(
         max_length=20,
-        choices=ROLE_CHOICES)
+        choices=Role.choices,
+        default=Role.CLIENT,
+    )
     
     def __str__(self):
         return f"{self.username} ({self.role})"
