112e15029e7b8b2da0a4cec24c818b51ad79ffdb
MahadSeecs <mahadm.bscs21seecs@seecs.edu.pk>
2025-12-17 18:16:16 +0500

    Changed CharField with TextChoices

--- tickets/models.py
+++ tickets/models.py
@@ -1,31 +1,29 @@
 from django.db import models
 from django.conf import settings
 
-
 User = settings.AUTH_USER_MODEL
-# Create your models here.
 
 
 class Ticket(models.Model):
-    STATUS_OPEN = "open"
-    STATUS_ASSIGNED = "assigned"
-    STATUS_RESOLVED = "resolved"
 
-    STATUS_CHOICES = [
-        (STATUS_OPEN, "Open"),
-        (STATUS_ASSIGNED, "Assigned"),
-        (STATUS_RESOLVED, "Resolved"),
-    ]
+    class Status(models.TextChoices):
+        OPEN = "open", "Open"
+        ASSIGNED = "assigned", "Assigned"
+        RESOLVED = "resolved", "Resolved"
 
     title = models.CharField(max_length=255)
     description = models.TextField()
 
     status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default=STATUS_OPEN
+        max_length=20,
+        choices=Status.choices,
+        default=Status.OPEN,
     )
 
     created_by = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="created_tickets"
+        User,
+        on_delete=models.CASCADE,
+        related_name="created_tickets",
     )
 
     assigned_to = models.ForeignKey(
@@ -37,4 +35,4 @@ class Ticket(models.Model):
     )
 
     def __str__(self):
-        return f"[{self.status}] {self.title}"
+        return f"[{self.get_status_display()}] {self.title}"
